29e95fcc63ea17b5efce4d38202aa7b57931608d
mangesori <vaporware08@gmail.com>
2024-12-03 23:55:46 +0800

    your commit message

--- src/app/quiz/level/page.js
+++ src/app/quiz/level/page.js
@@ -1,7 +1,13 @@
 "use client";
 
-import LevelQuiz from "@/components/sections/quiz/LevelQuiz";
+import dynamic from "next/dynamic";
 import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+
+// 동적 import로 클라이언트 사이드에서만 렌더링되도록 설정
+const LevelQuiz = dynamic(
+  () => import("@/components/sections/quiz/LevelQuiz"),
+  { ssr: false }
+);
 
 const LevelQuizPage = () => {
   return (
